84817e8259601e2f7d9541b28c54e0f2228c33c2
James Soucy <james8soucy@gmail.com>
2019-09-16 17:05:31 -0400

    completed task 2: write pretty print functions for j0

--- j0/j0.py
+++ j0/j0.py
@@ -1,13 +1,19 @@
 class JNumber:
     def __init__(self, _n):
         self.n = _n
+    def pp(self):
+        return str(self.n)
 
 class JPlus:
     def __init__(self, l, r):
         self.left = l
         self.right = r
+    def pp(self):
+        return "( " + self.l.pp() + " + " self.r.pp() + " )"
 
 class JMult:
     def __init__(self, l, r):
         self.left = l
         self.right = r
+    def pp(self):
+        return "( " + self.l.pp() + " * " self.r.pp() + " )"
