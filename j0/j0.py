edf3fa8eb3932bc6230e755297745427ee439870
James Soucy <james8soucy@gmail.com>
2019-09-16 17:40:15 -0400

    completed task 3: write 12 test suite cases for j0

--- j0/j0.py
+++ j0/j0.py
@@ -17,3 +17,20 @@ class JMult:
         self.right = r
     def pp(self):
         return "( " + self.l.pp() + " * " self.r.pp() + " )"
+        
+#step 3: j0 test suite
+
+test_values = [
+    JPlus(JNumber(6), JMult(JNumber(4), JNumber(3))), #18
+    JMult(JPlus(JNumber(9), JNumber(8)), JPlus(JNumber(2), JPlus(JNumber(3), JNumber(4)))), #153
+    JPlus(JNumber(4), JPlus(JNumber(4), JNumber(9))), #17
+    JMult(JPlus(JNumber(9), JNumber(4)), JNumber(11)), #143
+    JNumber(9), #9
+    JPlus(JNumber(4), JNumber(9)), #13 
+    JMult(JMult(JNumber(5), JNumber(9)), JMult(JNumber(3), JNumber(11))), #1485 
+    JPlus(JMult(JNumber(3), JNumber(9)), JNumber(2)), #29
+    JMult(JMult(JNumber(4), JNumber(2)), JNumber(8)), #64 
+    JPlus(JNumber(3), JMult(JNumber(5), JNumber(8))), #43
+    JPlus(JMult(JNumber(4), JNumber(3)), JNumber(13)), #25
+    JMult(JPlus(JNumber(3), JNumber(9)), JNumber(12)) #144
+]
