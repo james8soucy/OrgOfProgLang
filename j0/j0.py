de23007c3218c2b037a39deb8d2f0805ca1879fe
James Soucy <james8soucy@gmail.com>
2019-10-02 20:57:23 -0400

    fixed cases in desugar function based on examples shown in class on 9/19

--- j0/j0.py
+++ j0/j0.py
@@ -41,19 +41,28 @@ class SeCons:
 def desugar(sexpr):
     # e = v
     if (isinstance(sexpr, SeNum)):
-        # print('val')
         return JNumber(sexpr.n)
     # e = (+ e e)
-    if (isinstance(sexpr, SeCons) and isinstance(sexpr.l, SeStr) and sexpr.l.s == '+' and isinstance(sexpr.r, SeCons) and isinstance(sexpr.r.r, SeCons) and isinstance(sexpr.r.r.r, SeEmp)):
-        # print('plus')
-        return JPlus(desugar(sexpr.r.l), desugar(sexpr.r.r.l))
+    if (isinstance(sexpr, SeCons) and isinstance(sexpr.l, SeStr) and sexpr.l.s == '+' and isinstance(sexpr.r, SeCons)):
+        return JPlus(desugar(sexpr.r.l), desugar(SeCons(SeStr('+'), sexpr.r.r)))
     # e = (* e e)
-    if (isinstance(sexpr, SeCons) and isinstance(sexpr.l, SeStr) and sexpr.l.s == '*' and isinstance(sexpr.r, SeCons) and isinstance(sexpr.r.r, SeCons) and isinstance(sexpr.r.r.r, SeEmp)):
-        # print('mult')
-        return JMult(desugar(sexpr.r.l), desugar(sexpr.r.r.l))
-    print('case error')
+    if (isinstance(sexpr, SeCons) and isinstance(sexpr.l, SeStr) and sexpr.l.s == '*' and isinstance(sexpr.r, SeCons)):
+        return JMult(desugar(sexpr.r.l), desugar(SeCons(SeStr('*'), sexpr.r.r)))
+    # e = (+)
+    if (isinstance(sexpr, SeCons) and isinstance(sexpr.l, SeStr) and sexpr.l.s == '+' and isinstance(sexpr.r, SeEmp)):
+        return JNumber(0)
+    # e = (*)
+    if (isinstance(sexpr, SeCons) and isinstance(sexpr.l, SeStr) and sexpr.l.s == '*' and isinstance(sexpr.r, SeEmp)):
+        return JNumber(1)
+    # e = (- e)
+    if (isinstance(sexpr, SeCons) and isinstance(sexpr.l, SeStr) and sexpr.l.s == '-' and isinstance(sexpr.r, SeCons) and isinstance(sexpr.r.r, SeEmp)):
+        return JMult(JNumber(-1), desugar(sexpr.r.l))
+    # e = (- e e)
+    if (isinstance(sexpr, SeCons) and isinstance(sexpr.l, SeStr) and sexpr.l.s == '-' and isinstance(sexpr.r, SeCons) and isinstance(sexpr.r.r, SeCons) and isinstance(sexpr.r.r.r, SeEmp)):
+        return JPlus(desugar(sexpr.r.l), JMult(JNumber(-1), desugar(sexpr.r.r.l)))
+    return 'case error'
 
-expected = [18, 153, 17, 143, 9, 13, 1485, 29, 64, 43, 25, 144]
+expected = [18, 153, 17, 143, 9, 13, 1485, 29, 64, 43, 3, -5]
 
 test_values = [
     SeCons(SeStr('+'), SeCons(SeNum(6), SeCons(SeCons(SeStr('*'), SeCons(SeNum(4), SeCons(SeNum(3), SeEmp()))), SeEmp()))),
@@ -66,8 +75,8 @@ test_values = [
     SeCons(SeStr('+'), SeCons(SeCons(SeStr('*'), SeCons(SeNum(3), SeCons(SeNum(9), SeEmp()))), SeCons(SeNum(2), SeEmp()))),
     SeCons(SeStr('*'), SeCons(SeCons(SeStr('*'), SeCons(SeNum(4), SeCons(SeNum(2), SeEmp()))), SeCons(SeNum(8), SeEmp()))),
     SeCons(SeStr('+'), SeCons(SeNum(3), SeCons(SeCons(SeStr('*'), SeCons(SeNum(5), SeCons(SeNum(8), SeEmp()))), SeEmp()))),
-    SeCons(SeStr('+'), SeCons(SeCons(SeStr('*'), SeCons(SeNum(4), SeCons(SeNum(3), SeEmp()))), SeCons(SeNum(13), SeEmp()))),
-    SeCons(SeStr('*'), SeCons(SeCons(SeStr('+'), SeCons(SeNum(3), SeCons(SeNum(9), SeEmp()))), SeCons(SeNum(12), SeEmp())))
+    SeCons(SeStr('-'), SeCons(SeNum(9), SeCons(SeNum(6), SeEmp()))),
+    SeCons(SeStr('-'), SeCons(SeNum(5), SeEmp()))
 ]  
 
 for index, value in enumerate(test_values):
